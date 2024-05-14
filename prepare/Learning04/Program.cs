e83d36acde1c97e24b7642a0763cdc1eea47fa40
Dylan Stephenson <dylan.stephenson@studiesweekly.com>
2024-05-14 11:05:02 -0600

    completed inheritance homework assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment hw = new Assignment("Dylan", "Math");
+
+        MathAssignment mathHw = new MathAssignment("Dylan Stephenson", "Math", "7B", "1-10");
+
+        WritingAssignment writeHw = new WritingAssignment("Sophie Anne", "English", "Bridge Over Troubled Water");
+
+        Console.WriteLine(writeHw.GetSummary());
+        Console.WriteLine(writeHw.GetWritingInformation());
     }
 }
