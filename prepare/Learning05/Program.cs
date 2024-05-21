be200d35b305807473ca3bd300bf3b90c9d71aa4
Dylan Stephenson <dylan.stephenson@studiesweekly.com>
2024-05-21 12:55:21 -0600

    completed week 5 polymorphism activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("blue", 4);
+        Rectangle rectangle = new Rectangle("red", 6, 8);
+        Circle circle = new Circle("green", 7);
+
+        List<Shape> shapes = new List<Shape>() {square, rectangle, circle};
+
+        foreach (Shape shape in shapes)
+        {
+            string shapeColor = shape.GetColor();
+            double shapeArea = shape.GetArea();
+
+            Console.WriteLine($"The color of the shape is {shapeColor} and the area of the shape is {shapeArea}");
+        }
     }
 }
